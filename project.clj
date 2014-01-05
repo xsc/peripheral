ef6e8080ee4418d3d0145ef898f13aa15632421f
Yannick Scherer <yannick.scherer@gmail.com>
2014-01-05 13:17:33 +0100

    added Midje dependencies; using ':pedantic?'.

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.stuartsierra/component "0.2.1"]])
+                 [com.stuartsierra/component "0.2.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [joda-time]]
+                                  [joda-time "2.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}}
+  :aliases {"test-ancient" "midje"}
+  :pedantic? :abort)
