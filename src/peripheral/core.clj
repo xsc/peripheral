392bfdce1fb6b8aa5c95f26cddb6754fbb49ff48
Yannick Scherer <yannick.scherer@gmail.com>
2014-01-05 15:29:31 +0100

    using restructured namespaces; include API facade and subsystems.

--- src/peripheral/core.clj
+++ src/peripheral/core.clj
@@ -1,73 +1,30 @@
 (ns peripheral.core
   (:require [peripheral.configuration :refer [load-configuration!]]
-            [com.stuartsierra.component :as component]))
+            [peripheral.system-map :as sys]
+            [peripheral.subsystem :as sub]
+            [com.stuartsierra.component :as component]
+            [potemkin :refer [import-vars]]))
 
-;; ## Logic
+;; ## API Facade
 
-(defn connect
-  "Let the component identified by `src-id` be using the component identified
-   by `dst-id` as a dependency to be assoc'd in at the key `src-key`."
-  ([m src-id dst-id] (connect m src-id dst-id dst-id))
-  ([m src-id src-key dst-id]
-   (if-not (= src-id dst-id)
-     (assoc-in m [:dependencies src-id src-key] dst-id)
-     m)))
+(import-vars
+  [peripheral.system-map
+   connect configure]
+  [peripheral.subsystem
+   start-subsystem]
+  [com.stuartsierra.component
+   start stop])
 
-(defn with-config
-  "Associate a key in the component identified by `src-id` with the configuration stored
-   at `cfg-key`."
-  ([m src-id cfg-key] (with-config m src-id cfg-key cfg-key))
-  ([m src-id src-key cfg-key]
-   (assoc-in m [:configurations src-id src-key] cfg-key)))
+;; ## System Map Creation/Processing
 
-(defmacro on
-  "Register handlers."
-  [m k & fn-body]
-  `(assoc-in ~m [:on ~k] (fn ~@fn-body)))
-
-;; ## System
-
-(defn- global?
-  "Is the given symbol representing a global component?"
-  [sym]
-  (-> sym meta :global))
-
-(defn- config?
-  "Is the given symbol representing a configuration?"
-  [sym]
-  (-> sym meta :config))
-
-(defn initial-dependencies
-  "Create initial dependency map."
-  [m components]
-  (let [{:keys [local global]} (reduce
-                                 (fn [m sym]
-                                   (let [k (if (global? sym) :global :local)]
-                                     (update-in m [k] conj (keyword sym))))
-                                 {} components)]
-    (->> (for [a local b global] [a b])
-         (reduce #(apply connect %1 %2) m))))
-
-(defn initial-configurations
-  "Create initial configuration map."
-  [m components configurations]
-  (->> (for [a components b configurations]
-         [(keyword a) (keyword b)])
-       (reduce #(apply with-config %1 %2) m)))
-
-(defn attach-system-meta
-  "Attach system metadata to component record."
-  [system data]
-  (vary-meta system assoc ::data data))
-
-(defn using-configurations
+(defn system-configurations-using
   "Assoc configurations into the given System using the given configuration
    dependency map."
   [system configurations]
   (let [configuration-keys (->> (vals configurations)
                                 (mapcat vals)
                                 (set))
-        idle-configurations (map (juxt identity #(get system %)) configuration-keys)
+        idle-configurations (select-keys system configuration-keys)
         loaded-configurations (->> (for [[k cfg] idle-configurations]
                                      [k (load-configuration! cfg)])
                                    (into {}))]
@@ -81,39 +38,43 @@
 
 (defn start-system-with-meta
   "Start the given component using previously stored metadata."
-  [system component-keys]
-  (let [{:keys [dependencies configurations on]} (-> system meta ::data)
-        init (:start on identity)
-        stop (:stop on identity)]
+  [system]
+  (let [{:keys [dependencies configurations components]} (sys/system-meta system)]
     (-> system
-        init
-        (using-configurations configurations)
+        (system-configurations-using configurations)
         (component/system-using dependencies)
-        (component/start-system component-keys))))
+        (component/start-system components))))
 
 (defn stop-system-with-meta
   "Stop the given component using previously stored metadata."
-  [system component-keys]
-  (component/stop-system system component-keys)
-  (when-let [stop (-> system meta ::data :on :stop)]
-    (stop system)))
+  [system]
+  (let [{:keys [components]} (sys/system-meta system)]
+    (component/stop-system system components)))
+
+;; ## System Creation
 
 (defmacro defsystem
   "Create new system component record."
   [id fields & impl]
-  (let [components (remove config? fields)
-        component-keys (map keyword components)
-        configurations (filter config? fields)
+  (let [cfg-fn (symbol (str (name id) "->configuration"))
         [system-logic specifics] (split-with (complement symbol?) impl)]
-    `(defrecord ~id [~@fields]
-       component/Lifecycle
-       (start [this#]
-         (-> this#
-             (attach-system-meta
-               (-> ~(-> {}
-                        (initial-dependencies components)
-                        (initial-configurations components configurations))
-                   ~@system-logic))
-             (start-system-with-meta [~@component-keys])))
-       (stop [this#]
-         (stop-system-with-meta this# [~@component-keys])) ~@specifics)))
+    `(do
+       (defn ~cfg-fn
+         ~(str "Create peripheral system map for the given `" (name id) "` record.")
+         [{:keys [~@fields]}]
+         (-> ~(sys/initial-system-map fields)
+             ~@system-logic))
+       (defrecord ~id [~@fields]
+         component/Lifecycle
+         (start [this#]
+           (-> this#
+               (sys/initialize-system-meta ~cfg-fn)
+               (start-system-with-meta)))
+         (stop [this#]
+           (stop-system-with-meta this#))
+         sub/Subsystem
+         (start-subsystem [this# components#]
+           (-> this#
+               (sub/initialize-subsystem-meta ~cfg-fn components#)
+               (start-system-with-meta)))
+         ~@specifics))))
