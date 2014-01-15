56f50c2c80b2b483ecee228051a9e8df870d875b
Yannick Scherer <yannick.scherer@gmail.com>
2014-01-15 23:57:06 +0100

    added tests for protocol implementation in 'defcomponent'.

--- test/peripheral/component_test.clj
+++ test/peripheral/component_test.clj
@@ -3,22 +3,54 @@
             [peripheral.component :refer [defcomponent]]
             [com.stuartsierra.component :refer [start stop]]))
 
+;; ## Basic Functionality
+
 (defcomponent Test [n]
   :n-inc (inc n) dec
-  :n-twice (* n 2))
+  :n-twice (* n-inc 2))
 
 (fact "about 'defcomponent'"
-      (let [t (map->Test {:n 1})
+      (let [t (map->Test {:n 0})
             started (start t)
             stopped (stop started)]
         (instance? Test t) => truthy
         (keys t) => (contains #{:n :n-inc :n-twice})
-        (:n t) => 1
+        (:n t) => 0
         (:n-inc t) => nil
         (:n-twice t) => nil
-        (:n started) => 1
-        (:n-inc started) => 2
+        (:n started) => 0
+        (:n-inc started) => 1
         (:n-twice started) => 2
-        (:n stopped) => 1
-        (:n-inc stopped) => 1
+        (:n stopped) => 0
+        (:n-inc stopped) => 0
         (:n-twice stopped) => nil))
+
+;; ## Protocol Implementation
+
+(defprotocol Proto
+  (x [this]))
+
+(defcomponent Test [n]
+  :a n
+  :b (inc a)
+  Proto
+  (x [_] b))
+
+(fact "about 'defcomponent' + protocol"
+      (let [t (map->Test {:n 0})
+            started (start t)
+            stopped (stop started)]
+        (instance? Test t) => truthy
+        (satisfies? Proto t) => truthy
+        (keys t) => (contains #{:n :a :b})
+        (:n t) => 0
+        (:a t) => nil
+        (:b t) => nil
+        (:n started) => 0
+        (:a started) => 0
+        (:b started) => 1
+        (x started) => 1
+        (:n stopped) => 0
+        (:a stopped) => nil
+        (:b stopped) => nil
+        (x stopped) => nil))
