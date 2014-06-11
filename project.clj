8bcc4cf06e9ddec05efd7d056ba2a995cd53b88e
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-06-11 12:26:30 +0200

    Release 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peripheral "0.2.2-SNAPSHOT"
+(defproject peripheral "0.2.2"
   :description "System Creation for `stuartsierra/component`."
   :url "https://github.com/xsc/peripheral"
   :license {:name "Eclipse Public License"
