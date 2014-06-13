1877047d178301c3b4bb9db9e1e76d459019cb46
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-06-13 13:17:50 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.stuartsierra/component "0.2.1"]
                  [potemkin "0.3.4"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [joda-time]]
+  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
                                   [joda-time "2.3"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :aliases {"test" "midje"}
