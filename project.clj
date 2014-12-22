39fe709283847bde300c685852104808558d076a
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-12-22 17:33:00 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.stuartsierra/component "0.2.1"]
-                 [potemkin "0.3.4"]]
+                 [com.stuartsierra/component "0.2.2"]
+                 [potemkin "0.3.11"]]
   :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
-                                  [joda-time "2.3"]]
+                                  [joda-time "2.6"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :aliases {"test" "midje"}
   :pedantic? :abort)
