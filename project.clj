b120c0aae031260b9de7557ab16ee53c6df5e538
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-10-19 12:01:50 +0200

    Version 0.4.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peripheral "0.4.6"
+(defproject peripheral "0.4.7-SNAPSHOT"
   :description "System Creation for `stuartsierra/component`."
   :url "https://github.com/xsc/peripheral"
   :license {:name "Eclipse Public License"
