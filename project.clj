24f7846f8d8ddda1032f110eec5b1608b317ff91
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-02-12 16:37:27 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stuartsierra/component "0.3.0"]
-                 [potemkin "0.4.1"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+                 [com.stuartsierra/component "0.3.1"]
+                 [potemkin "0.4.3"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-midje "3.1.3"]]
                    :exclusions [org.clojure/clojure]}}
   :aliases {"test" "midje"
