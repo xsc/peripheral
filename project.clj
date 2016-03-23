ef4c9555986be24481cf8596d189019936ed686a
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-23 13:39:42 +0100

    use Clojure 1.8.0 as 'provided' version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/xsc/peripheral"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [com.stuartsierra/component "0.3.1"]
                  [potemkin "0.4.3"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
