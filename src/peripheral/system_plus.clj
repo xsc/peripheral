63fe5b242fddf00de3c651fcd762995a2bed5d36
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-23 15:34:33 +0100

    implement 'defsystem+'.
    tests are identical to 'defsystem'.

---
+++ src/peripheral/system_plus.clj
@@ -0,0 +1,123 @@
+(ns peripheral.system-plus
+  (:require [peripheral
+             [component :refer [defcomponent]]
+             [utils :refer [is-class-name?]]]
+            [peripheral.system
+             [analysis :as analysis]
+             [subsystem :as subsystem]]
+            [com.stuartsierra.dependency :as dep]
+            [com.stuartsierra.component :as component]))
+
+;; ## Startup
+
+(defn- assert-valid
+  [component system name]
+  (when-not (map? component)
+    (throw
+      (IllegalStateException.
+        (format "%s.%s > invalid component: %s"
+                (.getSimpleName (class system))
+                name
+                (pr-str component)))))
+  component)
+
+(defn- assert-dependencies-valid
+  [system component-name using]
+  (doseq [k (vals using)
+          :let [component (get system k)]]
+    (when (nil? component)
+      (let [sn (.getSimpleName (class system))]
+        (throw
+          (IllegalStateException.
+            (format "%s.%s is nil (depended on by %s.%s)"
+                    sn
+                    (name k)
+                    sn
+                    component-name))))))
+  system)
+
+(defn- prepare-components
+  [system components required?]
+  (reduce
+    (fn [system {:keys [key name default using]}]
+      (if (required? key)
+        (-> system
+            (assert-dependencies-valid name using)
+            (update key #(-> %
+                             (or default)
+                             (assert-valid system name)
+                             (component/using using))))
+        system))
+    system components))
+
+(defn start-system
+  [components system]
+  (let [active (or (subsystem/active-components system)
+                   (map :key components))
+        required? (analysis/required-components components active)]
+    (-> system
+       (prepare-components components required?)
+       (subsystem/set-active-components required?)
+       (component/start-system required?))))
+
+;; ## Shutdown
+
+(defn- clean-components
+  [system components]
+  (component/update-system-reverse
+    system
+    (filter #(get system %) components)
+    identity))
+
+(defn stop-system
+  [system]
+  (let [active (subsystem/active-components system)]
+    (-> system
+        (component/stop-system active)
+        (clean-components active))))
+
+;; ## Macro
+
+(doto
+  (defmacro ^{:added "0.5.0"} defsystem+
+    "Create a new system of components. Example:
+
+         (defsystem+ MySystem [^:global configuration, api-options]
+           :engine    []
+           :monitor   [:scheduler :api]
+           :api       [:engine {:options :api-options}]
+           :scheduler [:engine])
+
+     On startup, the following injections will happen:
+
+     - `:configuration` into `:engine`, `:api` and `:scheduler` (since it is
+       marked as `:global`),
+     - `:engine` into `:api` and `:scheduler`,
+     - `:api-options` - aliased as `:options` - into `:api`,
+     - `:scheduler` and `:api` into `:monitor`.
+
+     It is possible to give default values for each component using the `:default`
+     metadata:
+
+         (defsystem+ MySystem [...]
+           :engine ^{:default (default-engine)} []
+           ...)
+
+     Unless the key `:engine` is present and non-nil on system startup, it'll
+     be filled with the result of `(default-engine)`.
+     "
+    [id & args]
+    (let [{:keys [dependencies components specifics]} (analysis/analyze args)
+          fields (->> (map (comp symbol :name) components)
+                      (concat dependencies)
+                      (distinct))]
+      `(defcomponent ~id [~@fields]
+         :peripheral/start #(start-system [~@components] %)
+         :peripheral/stop  #(stop-system %)
+
+         ~@specifics)))
+  (alter-meta!
+    assoc
+    :arglists
+    '([id & components]
+      [id [dependency ...] & components])))
---
+++ test/peripheral/system_plus_test.clj
@@ -0,0 +1,107 @@
+(ns peripheral.system-plus-test
+  (:require [midje.sweet :refer :all]
+            [com.stuartsierra.component :as component]
+            [peripheral.system-plus :refer :all]
+            [peripheral.system.subsystem :refer [subsystem]]))
+
+;; ## Fixtures
+
+(defrecord X [started]
+  component/Lifecycle
+  (start [this]
+    (assoc this :started true))
+  (stop [this]
+    (assoc this :started false)))
+
+(def make-x
+  #(X. false))
+
+(defsystem+ Sys []
+  :g ^:global []
+  :a [:c]
+  :b [:c]
+  :c [])
+
+(def sys
+  (map->Sys
+    (->> (for [k [:g :a :b :c]]
+           [k (make-x)])
+         (into {}))))
+
+(defsystem+ SysWithData [^:global config, name]
+  :g ^:global []
+  :a {:system :name})
+
+(def sys2
+  (map->SysWithData
+    (->> (for [k [:g :a]]
+           [k (make-x)])
+         (into {})
+         (merge {:config :config-data
+                 :name :sys}))))
+
+;; ## Tests
+
+(fact "about the initial system state"
+      (let [{:keys [g a b c]} sys]
+        (map :started [g a b c]) => (has every? falsey)
+        (map :g [a b c]) => (has every? falsey)
+        (map :c [a b c]) => (has every? falsey))
+      (let [{:keys [g a config name]} sys2]
+        config => :config-data
+        name => :sys
+        (map :started [g a]) => (has every? falsey)
+        (map :config [g a]) => (has every? falsey)
+        (:g a) => falsey
+        (:system a) => falsey))
+
+(fact "about starting/stopping a simple system"
+      (let [{:keys [g a b c] :as started} (component/start sys)]
+        (map :started [g a b c]) => (has every? truthy)
+        (map :g [a b c]) => (has every? #{g})
+        (map :c [a b c]) => [c c nil]
+        (let [{:keys [g a b c] :as stopped} (component/stop started)]
+          (map :started [g a b c]) => (has every? falsey)
+          (map (comp :started :g) [a b c]) => (has every? falsey)
+          (map (comp :started :c) [a b]) => (has every? falsey))))
+
+(fact "about starting/stopping a complex system"
+      (let [{:keys [g a config name] :as started} (component/start sys2)]
+        config => :config-data
+        name => :sys
+        (map :started [g a]) => (has every? truthy)
+        (map :config [g a]) => (has every? #{:config-data})
+        (:g a) => g
+        (:system a) => :sys
+        (let [{:keys [g a config name] :as stopped} (component/stop started)]
+          config => :config-data
+          name => :sys
+          (map :started [g a]) => (has every? falsey)
+          (-> a :g :started) => falsey
+          (let [{:keys [g a config name] :as restarted} (component/start stopped)]
+            config => :config-data
+            name => :sys
+            (map :started [g a]) => (has every? truthy)
+            (map :config [g a]) => (has every? #{:config-data})
+            (:g a) => g
+            (:system a) => :sys))))
+
+(tabular
+  (fact "about starting/stopping subsystems"
+        (let [{:keys [g a b c] :as started}
+              (-> sys
+                  (subsystem ?components)
+                  (component/start))]
+          (map :started [g a b c]) => [?g ?a ?b ?c]
+          (map :g [a b c]) => [(when ?a g) (when ?b g) (when ?c g)]
+          (map (comp boolean :started :g) [a b c]) => [?a ?b ?c]
+          (map :c [a b c]) => [(when (and ?a ?c) c) (when (and ?b ?c) c) nil]
+          (let [{:keys [g a b c] :as stopped} (component/stop started)]
+            (map :started [g a b c]) => (has every? falsey)
+            (map (comp :started :g) [a b c]) => (has every? falsey))))
+  ?components         ?g     ?a    ?b    ?c
+  [:g]                true   false false false
+  [:a]                true   true  false true
+  [:b]                true   false true  true
+  [:a :b]             true   true  true  true
+  [:c]                true   false false true)
