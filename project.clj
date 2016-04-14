fec742aa62c9f65553ccf3618e1327a5dd158611
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-04-14 16:02:30 +0200

    Version 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peripheral "0.5.2-SNAPSHOT"
+(defproject peripheral "0.5.2"
   :description "Component & System Creation for Stuart Sierra's Component Library."
   :url "https://github.com/xsc/peripheral"
   :license {:name "MIT License"
