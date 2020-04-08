5eafb7d8fcb0afda1c81fa103a7467d486862203
Yannick Scherer <yannick.scherer@futurice.com>
2020-04-08 08:33:40 +0200

    Version 0.5.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject peripheral "0.5.4"
+(defproject peripheral "0.5.5-SNAPSHOT"
   :description "Component & System Creation for Stuart Sierra's Component Library."
   :url "https://github.com/xsc/peripheral"
   :license {:name "MIT License"
